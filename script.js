959a3a809c57e096836ef49280d59ec6af08b7cd
Thomas Giles <wthit56@googlemail.com>
2014-05-23 14:58:01 +0100

    by Urls
    Dynamic Count
    Tabs

--- script.js
+++ script.js
@@ -13,9 +13,15 @@
 }
 
 window.addEventListener("load", function () {
-	var byFile = (function () {
-		var byFile = document.getElementById("by-file");
-		var selection = byFile.selection = document.getElementById("by-file-selection-input");
+	function cancelSubmit(e) {
+		if (e.preventDefault) { e.preventDefault(); }
+	}
+
+	var byFiles = (function () {
+		var byFiles = document.getElementById("by-files");
+		byFiles.addEventListener("submit", cancelSubmit);
+
+		var selection = byFiles.selection = document.getElementById("by-files-selection-input");
 		
 		function inputChange() {
 			var files = this.files;
@@ -26,22 +32,30 @@ window.addEventListener("load", function () {
 			}
 
 			this.value = "";
+
+			updateCount();
 		}
 		selection.addEventListener("change", inputChange);
 
-		var selected = byFile.selected = document.getElementById("by-file-selected");
-		var clear = selected.clear = document.getElementById("by-file-selected-clear");
+		var selected = byFiles.selected = document.getElementById("by-files-selected");
+		var clear = selected.clear = document.getElementById("by-files-selected-clear");
 		var example = selected.example = selected.children[1];
 		var none = selected.children[0];
 
 		selected.removeChild(example);
+
+		var count = byFiles.count = document.getElementById("by-files-count");
+		function updateCount() {
+			var files = (selected.children[0] === none) ? 0 : selected.children.length;
+			count.innerText = files + " image" + ((files !== 1) ? "s" : "");
+		}
 
 		function addFile(file) {
 			if (selected.children[0] === none) { selected.removeChild(none); }
 
 			var previous = selected.children[selected.childElementCount - 1];
 
-			var cloned = selected.appendChild(example.cloneNode(true));
+			var cloned = example.cloneNode(true);
 			var clonedChildren = cloned.children;
 			clonedChildren[0].innerText = file.name;
 
@@ -56,6 +70,7 @@ window.addEventListener("load", function () {
 			clonedChildren[3].line = cloned;
 
 			cloned.file = file;
+			selected.appendChild(cloned);
 
 			clear.disabled = false;
 		}
@@ -97,16 +112,90 @@ window.addEventListener("load", function () {
 				clear.disabled = true;
 			}
 
+			updateCount();
+
 			return false;
 		}
 
 		function clearSelected() {
 			selected.innerHTML = "";
 			selected.appendChild(none);
+			updateCount();
 		}
 		clear.addEventListener("click", clearSelected);
 		clearSelected();
 
-		return byFile;
+		return byFiles;
 	})();
+
+	var byUrls = (function () {
+		var byUrls = document.getElementById("by-urls");
+		byUrls.addEventListener("submit", cancelSubmit);
+
+		var count = byUrls.count = document.getElementById("by-urls-count");
+
+		var input = byUrls.input = document.getElementById("by-urls-input");
+		var oldValue = input.value, next = null, debounce = 100;
+		function change() {
+			if (input.value !== oldValue) {
+				if (next) { clearTimeout(next); }
+				next = setTimeout(reCount, debounce);
+			}
+		}
+		var findUrls = /^(?:ftp|https?):\/\/.+$/gm;
+		function reCount() {
+			var urls = input.value.match(findUrls);
+			urls = urls ? urls.length : 0;
+			count.innerHTML = urls + " image" + ((urls !== 1) ? "s" : "");
+
+			next = null;
+			oldValue = input.value;
+		}
+		input.addEventListener("keydown", change);
+		input.addEventListener("keyup", change);
+		input.addEventListener("change", change);
+		input.addEventListener("paste", change);
+
+
+		return byUrls;
+	})();
+
+	var methods = (function () {
+		var methods = document.getElementById("methods");
+
+		var currentInput;
+		function select(e) {
+			var tab = e.target || e.srcElement;
+			if (currentInput !== tab.input) {
+				currentInput.style.display = "none";
+				tab.input.style.display = "block";
+				currentInput = tab.input;
+			}
+		}
+
+		var radios = methods.getElementsByTagName("INPUT");
+
+		methods.byFiles = radios[0];
+		methods.byFiles.input = byFiles;
+		methods.byFiles.addEventListener("change", select);
+
+		methods.byUrls = radios[1];
+		methods.byUrls.input = byUrls;
+		methods.byUrls.addEventListener("change", select);
+
+		var as = methods.getElementsByTagName("A");
+		function handleLink(e) {
+			if (e.preventDefault) { e.preventDefault(); }
+			this.parentNode.click();
+		}
+		as[0].addEventListener("click", handleLink);
+		as[1].addEventListener("click", handleLink);
+
+		byUrls.style.display = "none";
+		currentInput = byFiles;
+
+		return methods;
+	})();
+
+	methods.byUrls.click();
 });
